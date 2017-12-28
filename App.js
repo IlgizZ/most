4649045b51ee487e321b23262a01108d2733596d
Rinat Katipov <rinat777hatipov@gmail.com>
2017-12-28 16:07:34 +0600

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  View
+} from 'react-native';
+import LoginScene from './src/scenes/LoginScene'
+
+export default class App extends Component<{}> {
+
+  render() {
+    return (
+      <View>
+        <LoginScene />
+      </View>
+    );
+  }
+}
---
+++ src/scenes/LoginScene.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import  { View, Text } from 'react-native'
+class LoginScene extends React.Component {
+
+  render() {
+    console.log(123);
+    return(
+      <View>
+        <Text>
+          HEYqwe
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default LoginScene
